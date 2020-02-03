430fceca0ca946b90bedc6799a58c1fb029cb823
Björn Viklund <bjornbertilviklund@gmail.com>
2020-02-03 11:38:07 +0100

    Combined mount_sshfs.sh and start_node.sh into one.

--- uppmax_init/unmount_sshfs.sh
+++ uppmax_init/unmount_sshfs.sh
@@ -1,1 +1,6 @@
-fusermount -u mnt/usr/local/Modules ; fusermount -u mnt/sw ; fusermount -u mnt/proj ; fusermount -u  mnt/crex ; fusermount -u  mnt/home/*
+fusermount -u mnt/usr/local/Modules
+fusermount -u mnt/sw
+fusermount -u mnt/proj
+fusermount -u mnt/crex
+fusermount -u mnt/home
+fusermount -u mnt/etc
