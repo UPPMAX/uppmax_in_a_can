3bda91fadfd1e4c1520c1198f300638c22b5896d
dahlo <m.dahlo@gmail.com>
2020-02-03 16:31:55 +0100

    Updated documentation and help messages

--- uppmax_init/unmount_sshfs.sh
+++ uppmax_init/unmount_sshfs.sh
@@ -1,6 +1,6 @@
-fusermount -u mnt/usr/local/Modules
-fusermount -u mnt/sw
-fusermount -u mnt/proj
 fusermount -u mnt/crex
-fusermount -u mnt/home
 fusermount -u mnt/etc
+fusermount -u mnt/home
+fusermount -u mnt/proj
+fusermount -u mnt/sw
+fusermount -u mnt/usr/local/Modules
